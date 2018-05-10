f4f548cea9a7d657aecf3ff722cf977147e9c411
nakajob <35713978+nakajob@users.noreply.github.com>
2018-05-10 10:55:22 +0900

    fix bug

--- scripts/hector_motors.py
+++ scripts/hector_motors.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import sys, rospy, math, tf
 from pimouse_ros.msg import MotorFreqs
 from geometry_msgs.msg import Twist, Quaternion, TransformStamped, Point
@@ -25,9 +24,73 @@ class Motor():
 
         self.x, self.y, self.th = 0.0, 0.0, 0.0
         self.vx, self.vth = 0.0, 0.0
-
+        
         self.cur_time = rospy.Time.now()
         self.last_time = self.cur_time
+
+    def onoff_response(self, onoff):
+        d = TriggerResponse()
+        d.success = self.set_power(onoff)
+        d.message = "ON" if self.is_on else "OFF"
+        return d
+
+    def set_power(self, onoff = False):
+        en = "/dev/rtmotoren0"
+        try:
+            with open(en, 'w') as f:
+                f.write("1\n" if onoff else "0\n")
+            self.is_on = onoff
+            return True
+        except:
+            rospy.logerr("cannot write to " + en)
+
+        return False
+
+    def set_raw_freq(self, left_hz, right_hz):
+        if not self.is_on:
+            rospy.logerr("not enpowered")
+            return
+
+        try:
+            with open("/dev/rtmotor_raw_l0", 'w') as lf, open("/dev/rtmotor_raw_r0", 'w') as rf:
+                lf.write(str(int(round(left_hz))) + "\n")
+                rf.write(str(int(round(right_hz))) + "\n")
+        except:
+            rospy.logerr("cannot write to rtmotor_raw_*")
+
+    def callback_raw_freq(self, message):
+        self.set_raw_freq(message.left_hz, message.right_hz)
+    
+    def callback_cmd_vel(self, message):
+        if not self.is_on:
+            return
+        self.vx = message.linear.x
+        self.vth = message.angular.z
+
+        forward_hz = 80000.0*message.linear.x/(9*math.pi)
+        rot_hz = 400.0*message.angular.z/math.pi
+        self.set_raw_freq(forward_hz-rot_hz, forward_hz + rot_hz)
+        #self.using_cmd_vel = True
+        #self.last_time = rospy.Time.now()
+
+    def callback_on(self, message): return self.onoff_response(True)
+    def callback_off(self, message): return self.onoff_response(False)
+
+    def callback_tm(self, message):
+        if not self.is_on:
+            rospy.logerr("not enpowered")
+            return False
+
+        dev = "/dev/rtmotor0"
+        try:
+            with open(dev, 'w') as f:
+                f.write("%d %d %d\n" % (message.left_hz, message.right_hz, message.duration_ms))
+
+        except:
+            rospy.logerr("cannot write to " + dev)
+            return False
+
+        return True
 
     def send_odom(self):
         self.cur_time = rospy.Time.now()
@@ -35,17 +98,17 @@ class Motor():
         dt = self.cur_time.to_sec() - self.last_time.to_sec()
         self.x += self.vx * math.cos(self.th) * dt
         self.y += self.vx * math.sin(self.th) * dt
-        self.th += self.vth * dt 
+        self.th += self.vth * dt
 
         q = tf.transformations.quaternion_from_euler(0, 0, self.th)
-        self.bc_odom.sendTransform((self.x,self.y,0.0), q, self.cur_time,"base_frame","odom")
+        self.bc_odom.sendTransform((self.x, self.y, 0.0), q, self.cur_time, "base_frame", "odom")
 
         odom = Odometry()
         odom.header.stamp = self.cur_time
         odom.header.frame_id = "odom"
         odom.child_frame_id = "base_frame"
 
-        odom.pose.pose.position = Point(self.x,self.y,0)
+        odom.pose.pose.position = Point(self.x, self.y, 0)
         odom.pose.pose.orientation = Quaternion(*q)
 
         odom.twist.twist.linear.x = self.vx
@@ -55,70 +118,6 @@ class Motor():
         self.pub_odom.publish(odom)
 
         self.last_time = self.cur_time
-
-    def set_power(self,onoff=False):
-        en = "/dev/rtmotoren0"
-        try:
-            with open(en,'w') as f:
-                f.write("1\n" if onoff else "0\n")
-            self.is_on = onoff
-            return True
-        except:
-            rospy.logerr("cannot write to " + en)
-
-        return False
-
-    def set_raw_freq(self,left_hz,right_hz):
-        if not self.is_on:
-            rospy.logerr("not enpowered")
-            return
-
-        try:
-            with open("/dev/rtmotor_raw_l0",'w') as lf, open("/dev/rtmotor_raw_r0",'w') as rf:
-                lf.write(str(int(round(left_hz))) + "\n")
-                rf.write(str(int(round(right_hz))) + "\n")
-        except:
-            rospy.logerr("cannot write to rtmotor_raw_*")
-
-    def callback_raw_freq(self,message):
-        self.set_raw_freq(message.left_hz,message.right_hz)
-
-    def callback_cmd_vel(self,message):
-        if not self.is_on:
-            return
-        self.vx = message.linear.x
-        self.vth = message.angular.z
-
-        forward_hz = 80000.0*message.linear.x/(9*math.pi)
-        rot_hz = 400.0*message.angular.z/math.pi
-        self.set_raw_freq(forward_hz-rot_hz, forward_hz+rot_hz)
-        #self.using_cmd_vel = True
-        #self.last_time = rospy.Time.now()
-
-    def onoff_response(self,onoff):
-        d = TriggerResponse()
-        d.success = self.set_power(onoff)
-        d.message = "ON" if self.is_on else "OFF"
-        return d
-
-    def callback_on(self,message): return self.onoff_response(True)
-    def callback_off(self,message): return self.onoff_response(False)
-
-    def callback_tm(self,message):
-        if not self.is_on:
-            rospy.logerr("not enpowered")
-            return False
-
-        dev = "/dev/rtmotor0"
-        try:
-            with open(dev,'w') as f:
-                f.write("%d %d %d\n" %
-                    (message.left_hz,message.right_hz,message.duration_ms))
-        except:
-            rospy.logerr("cannot write to " + dev)
-            return False
-
-        return True
 
 if __name__ == '__main__':
     rospy.init_node('motors')
